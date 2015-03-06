e5ce69434cb3fbc7b072eac6a2273ba4a942737a
alexandder <aleksanderbolt@yahoo.com>
2015-03-06 04:18:09 +0100

    avg times

--- week2/Exercise1/src/main/java/exerciseone/ExerciseOne.java
+++ week2/Exercise1/src/main/java/exerciseone/ExerciseOne.java
@@ -5,7 +5,6 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Date;
-import java.util.concurrent.TimeUnit;
 
 class MyObject {
     public int x = 10;
@@ -37,22 +36,23 @@ public class ExerciseOne {
 
     public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         int N = 10;
-        int M = 100000;
+        int M = 1000000;
         int warmup = 10000000;
         long[] directAccessTimes = new long[N];
         long[] reflectionAccessTimes = new long[N];
 
         MyObject testObject = new MyObject(12);
-
+        int y;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < warmup; j++) {
-                //testObject.getX();
-                testObject.addToX(3);
+                y = testObject.x;
+                //testObject.addToX(3);
             }
             long startTime = System.nanoTime();
+
             for (int j = 0; j < M; j++) {
-                //testObject.getX();
-                testObject.addToX(3);
+                y = testObject.x;
+                //testObject.addToX(3);
             }
             long estimatedTime = System.nanoTime() - startTime;
             directAccessTimes[i] = estimatedTime;
@@ -63,29 +63,43 @@ public class ExerciseOne {
             Method method = testObject.getClass().getMethod("addToX", int.class);
             for (int j = 0; j < warmup; j++) {
 
-                //field.get(testObject);
-                method.invoke(testObject, 3);
+                field.get(testObject);
+                //method.invoke(testObject, 3);
             }
             long startTime = System.nanoTime();
             for (int j = 0; j < M; j++) {
 
-                //field.get(testObject);
-                method.invoke(testObject, 3);
+                field.get(testObject);
+                //method.invoke(testObject, 3);
             }
             long estimatedTime = System.nanoTime() - startTime;
             reflectionAccessTimes[i] = estimatedTime;
         }
 
+
+        double directAverage = 0;
+        double reflectionAverage = 0;
+
         System.out.println("Direct access (in seconds):");
         for (int i = 0; i < N; i++) {
+            directAverage += (directAccessTimes[i] / 1000000000.0);
             System.out.print(directAccessTimes[i] / 1000000000.0 + "\t");
         }
         System.out.println();
 
         System.out.println("Reflection access (in seconds):");
         for (int i = 0; i < N; i++) {
+            reflectionAverage += (reflectionAccessTimes[i] / 1000000000.0);
             System.out.print(reflectionAccessTimes[i] / 1000000000.0 + "\t");
         }
         System.out.println();
+
+        directAverage = directAverage / N;
+
+        reflectionAverage = reflectionAverage / N;
+
+        System.out.println("Average direct access (in seconds): " + directAverage);
+        System.out.println("Average reflection access (in seconds): " + reflectionAverage);
+        System.out.println("Ratio (average direct access to average reflection access): " + reflectionAverage/directAverage);
     }
 }
